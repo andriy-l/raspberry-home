19d5f4aa7faa3509f1cc0641b444e6a3db0d6265
Andriy Lutskiv <l.andriy@gmail.com>
2015-03-13 09:24:26 +0200

    small refactoring: move from examples dir

---
+++ src/ua/org/nalabs/intelhouse/CenterSystem.java
@@ -0,0 +1,16 @@
+package ua.org.nalabs.intelhouse;
+
+
+/**
+ * Головний клас, який ініціалізує всі класи
+ *
+ *
+ */
+public class CenterSystem{
+
+
+	public static void main(String... args){
+	System.out.println("Starting service...");
+	BlinkingLed bl = new BlinkingLed();
+	}
+}
