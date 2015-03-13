e8f292fb2a492f731d07752cd25b483b4764dbe3
Andriy Lutskiv <l.andriy@gmail.com>
2015-03-13 08:01:03 +0200

    Project just created

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # raspberry-home
+Temperature sensors manage system based on Raspberry PI
+Written on JAVA with PI4J library. Basedon PI4J examples.
+
---
+++ src/CenterSystem.java
@@ -0,0 +1,14 @@
+package ua.org.nalabs.intelhouse;
+
+/**
+ * Головний клас, який ініціалізує всі класи
+ *
+ *
+ */
+public class CenterSystem{
+
+
+	public static void main(String... args){
+	System.out.println("Starting service...");
+	}
+}
